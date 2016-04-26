508974695a374306d0551ebedf2d3e61251b1693
Stephen Kercher <skercher@purdue.edu>
2016-04-26 08:05:22 +0000

    Changed Index to better support backend;  added database communication

---
+++ db_submit.js
@@ -0,0 +1,29 @@
+/*eslint-env node, browser*/
+var express = require('express');
+var mysql =	require('mysql');
+
+var connection = mysql.createConnection({
+	host	: 'us-cdbr-iron-east-03.cleardb.net',
+	user	: 'b615dfccc67165',
+	password: '64fc33fa',
+	database: 'ad_38c35f1f5131ab4'
+});
+
+connection.connect(function(err) {
+	if (err){
+		console.error('Error Connecting: ' + err.stack);
+		return;
+	}
+});
+
+module.exports = connection;
+
+var db = connection;
+var db_submit = express();
+
+var sub_name = document.getElementById("submit_name");
+var sub_comment = document.getElementById("submit_comments");
+var sub_email = document.getElementById("submit_email");
+db.query('INSERT INTO volunteer (name, comment, email) VALUES (' + sub_name + ', ' + sub_comment + ', ' + sub_email + ')');
+
+connection.end();
